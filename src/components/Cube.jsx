6c7c859e9483bd103f64bb4ec8e4e0cf153b6d46
DisonUyoga <disonobudho233@gmail.com>
2024-02-14 15:00:58 +0300

    last

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -3,8 +3,13 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {
   faCss3, 
   faGitAlt, 
+  faDocker,
+  faJenkins,
   faHtml5, 
+  faNodeJs,
+  faFigma,
   faReact, 
+  faAws,
   faSass, 
   faVuejs} from '@fortawesome/free-brands-svg-icons'
 
@@ -13,22 +18,22 @@ const Cube = () => {
     <div className="stage-cube-cont">
           <div className="cubespinner">
             <div className="face1">
-              <FontAwesomeIcon icon={faVuejs} color="#008000" />
+              <FontAwesomeIcon icon={faJenkins} color="#008000" />
             </div>
             <div className="face2">
-              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+              <FontAwesomeIcon icon={faAws} color="#F06529" />
             </div>
             <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+              <FontAwesomeIcon icon={faDocker} color="#28A4D9" />
             </div>
             <div className="face4">
               <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
             </div>
             <div className="face5">
-              <FontAwesomeIcon icon={faSass} color="#DD0031" />
+              <FontAwesomeIcon icon={faNodeJs} color="#DD0031" />
             </div>
             <div className="face6">
-              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+              <FontAwesomeIcon icon={faFigma} color="#EC4D28" />
             </div>
           </div>
         </div>
